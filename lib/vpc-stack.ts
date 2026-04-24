30ccf2f22dca9fb4cabc43103ab3e53a837385e2
t_matsuoka@ap-com.co.jp <t_matsuoka@ap-com.co.jp>
2026-04-24 20:35:21 +0900

    change vpc setting

--- lib/vpc-stack.ts
+++ lib/vpc-stack.ts
@@ -15,11 +15,27 @@ export class VpcStack extends cdk.Stack {
     this.vpc = new ec2.Vpc(this, 'MyVpc', {
       ipAddresses: ec2.IpAddresses.cidr(props.cidr),
       maxAzs: 2,
-      natGateways: 1, // プライベートサブネットから外に出るためのゲートウェイ
+      natGateways: 1, 
       subnetConfiguration: [
         { name: 'Public', subnetType: ec2.SubnetType.PUBLIC },
         { name: 'Private', subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
       ],
+    });
+
+    // 1. ECR API用 (認証トークンの取得に必要)
+    this.vpc.addInterfaceEndpoint('EcrApiEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR,
+    });
+
+    // 2. ECR Docker用 (実際のイメージデータのプルに必要)
+    this.vpc.addInterfaceEndpoint('EcrDockerEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+    });
+
+    // 3. S3用 (ECRのイメージデータの実体はS3にあるため、これがないとプルが止まります)
+    // S3は「Gateway型」という無料のエンドポイントが使えます。
+    this.vpc.addGatewayEndpoint('S3Endpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.S3,
     });
   }
 }
